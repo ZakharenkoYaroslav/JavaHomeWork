32f9e2bc533742a34fe83b895db09a0de828b78b
Yaroslav Zakharenko <zakharenko.yaroslav@gmail.com>
2021-11-07 01:20:07 +0200

    init commit

--- src/com/pb/zakharenko/hw6/VetClinic.java
+++ src/com/pb/zakharenko/hw6/VetClinic.java
@@ -1,14 +1,27 @@
 package com.pb.zakharenko.hw6;
-use Veterinarian;
+import java.lang.reflect.Constructor;
+
+
+
+
 
 
 public class VetClinic {
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws Exception {
 
         Dog dog = new Dog("короткошерсна");
         Cat cat = new Cat("перська","Пенелопа");
         Horse horse = new Horse("короткогрива");
 
+        Class Veterinarian = Class.forName("com.pb.zakharenko.hw6.Veterinarian");
+        // создание объекта через рефлексию
+        Constructor constr = Veterinarian.getConstructor(new Class[]{String.class});
+        Object obj = constr.newInstance("Ветеринар");
+
+        if (obj instanceof Veterinarian) {
+            for (Animal animal : animals)
+                ((Veterinarian) obj).treatAnimal(animal);
+        }
 
 
         Animal[] animals = new Animal[] {dog, cat, horse};
--- src/com/pb/zakharenko/hw6/Veterinarian.java
+++ src/com/pb/zakharenko/hw6/Veterinarian.java
@@ -1,24 +1,12 @@
 package com.pb.zakharenko.hw6;
 
-import java.lang.reflect.Constructor;
-import java.util.Arrays;
+public class Veterinarian {
 
-public class Main {
-    public static void main(String[] args) throws Exception {
 
-        Class Veterinarian = Class.forName("Animal.Veterinarian");
-        // создание объекта через рефлексию
-        Constructor constr = Veterinarian .getConstructor(new Class[]{String.class});
-        Object obj = constr.newInstance("Петров");
-
-        public static void treatAnimal(Animal animal){
-            System.out.println(animal.getName());
-            System.out.println(animal.getLocation);
-            System.out.println(animal.getFood);
-        }
-
+    public void treatAnimal(Animal animal) {
+        System.out.println("-----------------------------------------");
+        System.out.println(animal.getName());
+        System.out.println(animal.getLocation());
+        System.out.println(animal.getFood());
     }
-
-
-
-    }
+}
